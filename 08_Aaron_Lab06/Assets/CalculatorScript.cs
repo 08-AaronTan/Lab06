7c6ea8cc636db580336419bea65c5895493bad85
02-TeoKaiTeng <91868652+02-TeoKaiTeng@users.noreply.github.com>
2021-10-18 23:40:07 +0700

    Update Calculator Script

--- 08_Aaron_Lab06/Assets/CalculatorScript.cs
+++ 08_Aaron_Lab06/Assets/CalculatorScript.cs
@@ -11,6 +11,10 @@ public class CalculatorScript : MonoBehaviour
 
     public Toggle toggleUSDollar;
     public Toggle toggleJapaneseYen;
+    public Toggle toggleRM;
+    public Toggle toggleEUR;
+    public Toggle toggleKoreanWon;
+    public Toggle toggleTaiwanDollar;
 
     public InputField Amount;
     public InputField Value;
@@ -20,6 +24,10 @@ public class CalculatorScript : MonoBehaviour
 
     private float SGDUSD_rate = 0.74f;
     private float SGDJPY_rate = 82.79f;
+    private float RM = 3.08f;
+    private float EUR = 0.63f;
+    private float KoreanWon = 881.54f;
+    private float TaiwanDollar = 20.73f;
 
     // Start is called before the first frame update
     void Start()
@@ -43,13 +51,60 @@ public class CalculatorScript : MonoBehaviour
         if(toggleUSDollar.isOn)
         {
             storeNumber = storeNumber * SGDUSD_rate;
-            toggleJapaneseYen.isOn = false;
+            toggleJapaneseYen.isOn = false;
+            toggleRM.isOn = false;
+            toggleEUR.isOn = false;
+            toggleKoreanWon.isOn = false;
+            toggleTaiwanDollar.isOn = false;
         }
 
         else if (toggleJapaneseYen.isOn)
         {
             storeNumber = storeNumber * SGDJPY_rate;
             toggleUSDollar.isOn = false;
+            toggleRM.isOn = false;
+            toggleEUR.isOn = false;
+            toggleKoreanWon.isOn = false;
+            toggleTaiwanDollar.isOn = false;
+        }
+
+        else if (toggleRM.isOn)
+        {
+            storeNumber = storeNumber * RM;
+            toggleUSDollar.isOn = false;
+            toggleJapaneseYen.isOn = false;
+            toggleEUR.isOn = false;
+            toggleKoreanWon.isOn = false;
+            toggleTaiwanDollar.isOn = false;
+        }
+        else if (toggleEUR.isOn)
+        {
+            storeNumber = storeNumber * EUR;
+            toggleUSDollar.isOn = false;
+            toggleJapaneseYen.isOn = false;
+            toggleRM.isOn = false;
+            toggleKoreanWon.isOn = false;
+            toggleTaiwanDollar.isOn = false;
+        }
+        else if (toggleKoreanWon.isOn)
+        {
+            storeNumber = storeNumber * KoreanWon;
+            toggleUSDollar.isOn = false;
+            toggleJapaneseYen.isOn = false;
+            toggleRM.isOn = false;
+            toggleEUR.isOn = false;
+            toggleTaiwanDollar.isOn = false;
+        }
+
+        else if (toggleTaiwanDollar.isOn)
+        {
+            storeNumber = storeNumber * TaiwanDollar;
+            toggleUSDollar.isOn = false;
+            toggleJapaneseYen.isOn = false;
+            toggleRM.isOn = false;
+            toggleEUR.isOn = false;
+            toggleKoreanWon.isOn = false;
+
         }
 
         else
@@ -68,6 +123,10 @@ public class CalculatorScript : MonoBehaviour
 
         toggleUSDollar.isOn = false;
         toggleJapaneseYen.isOn = false;
-
+        toggleRM.isOn = false;
+        toggleEUR.isOn = false;
+        toggleKoreanWon.isOn = false;
+        toggleTaiwanDollar.isOn = false;
+        print("");
     }
 }
